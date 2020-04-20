f735687787520610cd4422c05b5a8b227f665046
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-20 21:27:57 +0700

    fix(model): change data mapping and return value

--- src/model.py
+++ src/model.py
@@ -11,17 +11,19 @@ class WeatherSpecificLocation:
 
     def get_current(self):
         self.__fetch_data()
+        current_data = self.data['current']
         data_dict = dict()
-        data_dict['temp'] = self.data['temp']
-        data_dict['pressure'] = self.data['pressure']
-        data_dict['humidity'] = self.data['humidity']
-        data_dict['dew_point'] = self.data['dew_point']
-        data_dict['uvi'] = self.data['uvi']
-        data_dict['clouds'] = self.data['clouds']
-        data_dict['visibility'] = self.data['visibility']
-        data_dict['wind_speed'] = self.data['wind_speed']
-        data_dict['wind_deg'] = self.data['wind_deg']
-        data_dict['weather'] = self.data['weather'][0]['main']
+        data_dict['temp'] = current_data['temp']
+        data_dict['pressure'] = current_data['pressure']
+        data_dict['humidity'] = current_data['humidity']
+        data_dict['dew_point'] = current_data['dew_point']
+        data_dict['uvi'] = current_data['uvi']
+        data_dict['clouds'] = current_data['clouds']
+        data_dict['visibility'] = current_data['visibility']
+        data_dict['wind_speed'] = current_data['wind_speed']
+        data_dict['wind_deg'] = current_data['wind_deg']
+        data_dict['weather'] = current_data['weather'][0]['main']
+        return data_dict
 
     def __fetch_data(self):
         path = "data/2.5/onecall?lat={}&lon={}&appid={}".format(self.lat, self.lon, self.api_key)
