632800f0ae50c970bf87338578ed97ca1b6bff5b
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-20 21:20:26 +0700

    fix(cmd): change port flag

--- src/cmd.py
+++ src/cmd.py
@@ -3,8 +3,8 @@ import argparse
 
 def parse():
     parser = argparse.ArgumentParser(description='Run service')
-    parser.add_argument('--sum',
-                        '-s',
+    parser.add_argument('--port',
+                        '-p',
                         dest='port',
                         default=8081,
                         help='Port which service will listen to')
