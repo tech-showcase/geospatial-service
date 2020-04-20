44e532426f42204df23606229799e40c759487fc
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-20 20:54:37 +0700

    style(config): replace double quote with single quote

--- src/config.py
+++ src/config.py
@@ -4,13 +4,13 @@ from json import load
 
 class Config:
     def __init__(self):
-        environment = environ.get("ENVIRONMENT")
+        environment = environ.get('ENVIRONMENT')
         if environment is None:
-            environment = "DEV"
+            environment = 'DEV'
 
-        config_path = environ.get("{}_CONFIG_PATH".format(environment))
+        config_path = environ.get('{}_CONFIG_PATH'.format(environment))
         if config_path is None:
-            config_path = "config/config-dev.json"
+            config_path = 'config/config-dev.json'
 
         with open(config_path, 'r') as file:
             self.data = load(file)
