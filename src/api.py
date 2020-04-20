295273ed0d889947d3465f98b24d8da04a443790
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-20 21:17:45 +0700

    feat(api): add fetch current weather api handler

--- src/api.py
+++ src/api.py
@@ -1,8 +1,17 @@
 from flask import Blueprint
+import model
+import controller
+import global_var
 
 api = Blueprint('api', __name__)
 
 
 @api.route('/api/weather/current')
 def fetch_current_weather(*args, **kwargs):
-    return "dummy"
+    config = global_var.config.get()['weather']
+    weather_model = model.WeatherSpecificLocation(config['server_address'],
+                                                  config['api_key'],
+                                                  "-6.200000",
+                                                  "106.816666")
+    result_dict = controller.fetch_current_weather(weather_model)
+    return result_dict
