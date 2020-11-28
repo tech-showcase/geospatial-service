f673ca6aeb5b221904823d1ac94bcd2dd31cf85e
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-11-29 00:44:05 +0700

    feat(api): get lat and lon value from query params

--- src/api.py
+++ src/api.py
@@ -1,17 +1,22 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from src import model
 from src import controller
-from src import global_var
+from src import config as global_config
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/api/weather/current')
+@api.route('/api/weather/current', methods=['GET'])
 def fetch_current_weather(*args, **kwargs):
-    config = global_var.config.get()['weather']
+    config = global_config.config.get()['weather']
+
+    lat = request.args.get('lat', default='0')
+    lon = request.args.get('lon', default='0')
+
     weather_model = model.WeatherSpecificLocation(config['server_address'],
                                                   config['api_key'],
-                                                  '-6.200000',
-                                                  '106.816666')
+                                                  lat,
+                                                  lon)
     result_dict = controller.fetch_current_weather(weather_model)
+
     return jsonify(result_dict)
